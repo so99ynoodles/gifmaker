871b63899da623f2f22e36fdfd49f67ca34dee78
TAIKI IKEDA <taiki_ikeda@relationsgroup.co.jp>
2020-11-18 10:22:47 +0900

    Finish on gifmaker

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useMemo, useState } from 'react';
+import { useConvertToGif } from './hooks/useConvertToGif';
 import './App.css';
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
+  const [video, setVideo] = useState<File | null>();
+  const videoUrl = useMemo(() => (video ? URL.createObjectURL(video) : null), [
+    video,
+  ]);
+
+  const [gif, setGif] = useState<string>();
+  const [startTime, setStartTime] = useState<string>('2.0');
+  const [totalTime, setTotalTime] = useState<string>('2.5');
+  const { ready, convertToGif } = useConvertToGif();
+
+  async function handleConvert() {
+    const url = await convertToGif(video!, totalTime, startTime);
+    setGif(url);
+  }
+
+  return ready ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <div className="App__Media">
+        {video && (
+          <video
+            className="App__Media__Video"
+            controls
+            width="300"
+            src={videoUrl!}
+          />
+        )}
+      </div>
+      <input
+        className="App__Input"
+        type="file"
+        onChange={(e) => setVideo(e.target.files?.item(0))}
+      />
+
+      {video && (
+        <div className="App__Controls">
+          <div>
+            <label>total time</label>
+            <input
+              type="number"
+              step="0.1"
+              value={totalTime}
+              onChange={(e) => setTotalTime(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>start time</label>
+            <input
+              type="number"
+              step="0.1"
+              value={startTime}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
+          </div>
+        </div>
+      )}
+
+      <h3>Result</h3>
+      <button onClick={handleConvert}>Convert</button>
+      {gif && <img src={gif} alt="converted gif" />}
     </div>
+  ) : (
+    <div>loading...</div>
   );
 }
 
---
+++ src/hooks/useConvertToGif.tsx
@@ -0,0 +1,45 @@
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { useEffect, useState } from 'react';
+const ffmpeg = createFFmpeg({ log: true });
+
+export function useConvertToGif() {
+  const [ready, setReady] = useState(false);
+
+  async function load() {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function convertToGif(
+    video: File,
+    totalTime: string = '2.5',
+    startTime: string = '2.0',
+  ) {
+    ffmpeg.FS('writeFile', video.name, await fetchFile(video));
+    await ffmpeg.run(
+      '-i',
+      video.name,
+      // 動画の長さ
+      '-t',
+      totalTime,
+      // 動画のスタートタイム
+      '-ss',
+      startTime,
+      '-f',
+      'gif',
+      'out.gif',
+    );
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    return URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+  }
+
+  return {
+    ready,
+    convertToGif,
+  };
+}
